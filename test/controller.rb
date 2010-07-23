b861d21dfd6bd965ff7f92496295fa59e2020a0f
Steven Vetzal <steve@vetzal.com>
2010-07-23 11:27:15 -0400

    Seeing how some evals might look

--- test/controller.rb
+++ test/controller.rb
@@ -2,4 +2,12 @@ class Controller
   
   # This class will do some Actor / Subject evaluations
 
+  def create
+    if @user.can? :edit, :item
+      # Hunky Dory
+    else
+      # Security violation
+    end
+  end
+
 end
