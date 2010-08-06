f5bfacdf9ee7f177913b8704f93ea021ed568aa6
Steven Vetzal <steve@vetzal.com>
2010-08-06 17:17:54 -0400

    adding spec to test actual permission structures

--- spec/subject_spec.rb
+++ spec/subject_spec.rb
@@ -13,6 +13,25 @@ describe Cannibal::Subject do
 
     it { should respond_to(:allow) }
     it { should respond_to(:deny) }
+    it { should respond_to(:permissions) }
+    it { should respond_to(:permissions_for) }
+  end
+
+  context "when a simple permission is set" do
+    before(:all) do
+      class FooActor
+        include Cannibal::Actor
+      end
+      class FooSubject
+        include Cannibal::Subject
+        allow FooActor, :edit, :name
+      end
+    end
+
+    subject { FooSubject }
+
+    it { FooActor.new.can?(FooSubject, :edit, :name).should be_true }
+    it { FooActor.new.can?(FooSubject, :edit, :phone).should be_false }
   end
 
 end
