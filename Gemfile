bfb36bc654fac69e37117b5994be297d271a2bc7
Nate Smith <nwjsmith@gmail.com>
2010-08-06 19:13:56 -0400

    Added rake to bundle

--- Gemfile
+++ Gemfile
@@ -1,5 +1,7 @@
 source :rubygems
 
+gem 'rake'
+
 group :test do
   gem 'autotest'
   gem 'rspec', '2.0.0.beta.19'
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,10 +1,6 @@
 lib_dir = File.dirname(__FILE__) + '/../lib'
-
-require 'rspec'
 $:.unshift lib_dir unless $:.include?(lib_dir)
 
-require 'cannibal'
+require 'rspec'
 
-RSpec.configure do |config|
-  config.mock_with :rspec
-end
+require 'cannibal'
