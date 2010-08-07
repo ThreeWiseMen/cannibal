5a4ba228c14889f1cb2bda93a9037617c43ad2df
Steven Vetzal <steve@vetzal.com>
2010-08-07 11:57:53 -0400

    fixed up rspec tests, moving on to cucumber

--- test/user.rb
+++
@@ -1,7 +0,0 @@
-class User
-  extend Cannibal::Actor
-
-  # This class will fill the Actor role in an Actor / Subject evaluation
-  # Declarations for permissions should be specified on the Subject
-
-end
