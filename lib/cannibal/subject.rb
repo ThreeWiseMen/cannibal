0d2e60ce100d15783fcf4715aa1c6667ff0fba01
Nate Smith <nwjsmith@gmail.com>
2010-08-23 20:09:34 -0400

    Basic allow macro

--- lib/cannibal/subject.rb
+++ lib/cannibal/subject.rb
@@ -2,6 +2,11 @@ module Cannibal
 
   module Subject
     module ClassMethods
+      def allow(actor, verb)
+        method_name = "can_#{actor.to_s.downcase}_#{verb}?"
+        method_body = Proc.new { instance_eval { true } }
+        define_method(method_name, method_body)
+      end
     end
 
     def self.included(base)
--- spec/subject_spec.rb
+++ spec/subject_spec.rb
@@ -1,4 +1,27 @@
 require 'spec_helper'
 
 describe Cannibal::Subject do
+  before(:each) do
+    class Eater
+    end
+
+    class Victim
+      include Cannibal::Subject
+    end
+  end
+  subject { Victim }
+
+  it { should respond_to(:allow) }
+
+  describe ".allow" do
+    before(:each) { subject.allow Eater, :eat }
+
+    it "should define a method to check if an object can act on a Subject instance" do
+      subject.new.should respond_to(:can_eater_eat?)
+    end
+
+    it "should return true by default" do
+      subject.new.can_eater_eat?.should be_true
+    end
+  end
 end
